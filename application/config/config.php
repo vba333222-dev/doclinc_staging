dc794fce22751f17979956573bdcb31811f3f98d
vba333222-dev <ajie.aangga@gmail.com>
2026-06-15 21:03:08 +0700

    Fix session & config

--- application/config/config.php
+++ application/config/config.php
@@ -237,7 +237,7 @@ $config['log_threshold'] = 1;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = APPPATH . 'logs/';
 
 /*
 |--------------------------------------------------------------------------
@@ -296,7 +296,7 @@ $config['error_views_path'] = '';
 | application/cache/ directory.  Use a full server path with trailing slash.
 |
 */
-$config['cache_path'] = '';
+$config['cache_path'] = APPPATH . 'cache/';
 
 /*
 |--------------------------------------------------------------------------
@@ -387,7 +387,7 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'cilegon_bersatu_session';
 // $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = APPPATH . 'cache/sessions';  // Pastikan path ini benar
+$config['sess_save_path'] = APPPATH . 'cache/sessions/';  // Absolute writable path for CI3 file sessions.
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
