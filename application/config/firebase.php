417f736711d3f7de089bfb2928292e2f9d94e84f
Angga Ajie <job.anggaajie@gmail.com>
2026-06-20 05:41:26 +0700

    Move Firebase service account configuration to environment

--- application/config/firebase.php
+++ application/config/firebase.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['firebase_service_account'] = APPPATH . 'third_party/firebase/otp-sms-cilegon-firebase-adminsdk-heqye-e36414f974.json';
-$config['firebase_device_token'] = 'etssYYTHeeenGsjWoYJHeF:APA91bGFRjMMNeAOdkCRRNgxdVLATEDCkrFidhXOO0l73n44iFvxOiqC38pGBqk6njpflhZSMxuzjRdABIjai01BXq03f_w6Gfy5yoI0TYbORdBrC0JjNNg';
+$config['firebase_service_account'] = getenv('FIREBASE_SERVICE_ACCOUNT_PATH') ?: false;
+$config['firebase_device_token'] = getenv('FIREBASE_DEVICE_TOKEN') ?: '';
